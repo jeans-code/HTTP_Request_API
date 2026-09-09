34c1490b42e7f5e4a80042c6710c6823a0d0432e
jeans-code <jeanunder6@gmail.com>
2026-09-09 09:54:18 -0500

    Add basic structure to GetRequests.ts

--- netlify/functions/GetRequests.ts
+++ netlify/functions/GetRequests.ts
@@ -1,1 +1,8 @@
+import type { Config, Context } from "@netlify/functions";
 
+export default async (req: Request, context: Context) => {
+}
+
+export const config: Config = {
+  path: "/api/simple/GetRequest",
+}

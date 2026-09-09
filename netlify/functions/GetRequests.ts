ab375aa988c1a63ad1b442f65766f8014d2ce849
jeans-code <jeanunder6@gmail.com>
2026-09-09 14:52:48 -0500

    Addd axios types import

--- netlify/functions/GetRequests.ts
+++ netlify/functions/GetRequests.ts
@@ -1,8 +1,10 @@
 import type { Config, Context } from "@netlify/functions";
 import axios from "axios";
+import type { AxiosResponse, AxiosPromise, AxiosError } from "axios";
 
 
 export default async (req: Request, context: Context) => {
+  
 }
 
 export const config: Config = {

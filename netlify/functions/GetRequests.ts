873df4804f2d1b4f963600597d9e315254e02d9b
jeans-code <jeanunder6@gmail.com>
2026-09-11 13:37:49 -0500

    Update GetRequests.ts

--- netlify/functions/GetRequests.ts
+++ netlify/functions/GetRequests.ts
@@ -8,9 +8,13 @@ export default async (req: Request, context: Context) => {
   //axios.defaults.maxBodyLength = 20 * 1024 * 1024; // 20 MB
 
   // let c
-  let url;
+  // let url;
+  let url: URL | null | undefined;
   try {
     url = new URL(req.url);
+  catch (err) {
+    let res = new Response(`Invalid 
+  }
   
   
 
